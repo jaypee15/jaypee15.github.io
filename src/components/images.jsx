6d7a70b9a4e991540a15a08c7cc53099d79ae6a3
Johnpaul Okoye <okoyejohnpaul15@gmail.com>
2025-12-03 20:30:10 +0100

    update root

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 
 export const RideByImg = () => (
-  <img src="/portfolio/ride-by.png" alt="Ride-By " />
+  <img src="/jaypee15.github.io/ride-by.png" alt="Ride-By " />
 );
 export const StudyroomsImg = () => (
-  <img src="/portfolio/Study Rooms home.png" alt="Study Rooms web app homepage" />
+  <img src="/jaypee15.github.io/Study Rooms home.png" alt="Study Rooms web app homepage" />
 
 );
 
 export const EvotingImg = () => (
-  <img src="/portfolio/e-voting.jpeg" alt=":E voting-application screenshot" />
+  <img src="/jaypee15.github.io/e-voting.jpeg" alt=":E voting-application screenshot" />
 );
 
 
 export const GeoScopeImg = () => (
-  <img src="/portfolio/geoscope.png" alt="GeoScope" />
+  <img src="/jaypee15.github.io/geoscope.png" alt="GeoScope" />
 );
 export const SocialImg = () => (
-  <img src="/portfolio/fastapi-social.png" alt="FAstAPI Social API screenshot" />
+  <img src="/jaypee15.github.io/fastapi-social.png" alt="FAstAPI Social API screenshot" />
 );
 export const ShoesJetImg = () => (
-  <img src="/portfolio/shoes-jet.vercel.app.png" alt="Shoes Jet screenshot" />
+  <img src="/jaypee15.github.io/shoes-jet.vercel.app.png" alt="Shoes Jet screenshot" />
 );
